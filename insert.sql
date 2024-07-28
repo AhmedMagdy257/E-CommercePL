5224ceb9f71e68be59a88e0c24fc8d99cba0dc0a
Ahmed Magdy <ahmdmgdy72@gmail.com>
2024-07-28 15:31:40 +0300

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,12 @@
+insert into Products
+([Name],Discription,Price,Stock) 
+values
+('Formal Shirt','White Mandarin Trendy Shirt',400,60),
+('Sweatshirt','Side Printing Kangaroo Pocket Hoodie - Burgundy',600,95),
+('Pullover','Fancy V Neck Basic Pullover',750,75),
+('Hoody','AE Super Soft Fleece Graphic Hoodie',300,40),
+('Sweatpant','Elastic Waist With Metal Accessories Baggy Sweatpants Black',450,80),
+('Straight Jean','Ripped Fit Jeans',500,90),
+('Slim Jean','Back Pockets Plain Slim Fit Jeans Pants',600,110),
+('Bomber Jacket','Band Neck Leather Jacket With Pockets genuine leather',800,120),
+('Denim Jacket','Denim Jacket - Blue - Regular fit',650,70)
